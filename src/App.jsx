ea40c93c3863c7b9a11548c63c7e0f376b22b81c
kevin barany <kevbarany@hotmail.com>
2025-12-02 16:42:20 -0500

    set up post processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, Suspense } from "react";
 import Experience from "./Components/Experience";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  Bloom,
+  EffectComposer,
+  Vignette,
+  DepthOfField,
+  ToneMapping,
+} from "@react-three/postprocessing";
+import {
+  BlurPass,
+  Resizer,
+  KernelSize,
+  Resolution,
+  BlendFunction,
+  ToneMappingMode,
+} from "postprocessing";
+import { PerspectiveCamera } from "@react-three/drei";
 import "./App.css";
+import * as THREE from "three";
 
 function App() {
   return (
     <>
-      <Canvas>
-        <Experience />
+      <Canvas
+        camera={{ fov: 50 }}
+        gl={{
+          antialias: true,
+
+          toneMappingExposure: 0.5,
+        }}
+        onCreated={({ gl }) => {
+          gl.shadowMap.enabled = true;
+          gl.shadowMap.type = THREE.PCFSoftShadowMap;
+        }}
+        shadows
+      >
+        <Suspense fallback={null}>
+          <Experience />
+          <EffectComposer>
+            <Bloom
+              intensity={0.55}
+              luminanceThreshold={0.78}
+              luminanceSmoothing={0.15}
+              mipmapBlur={true}
+              radius={0.85}
+            />
+            <Vignette eskil={false} offset={0.2} darkness={0.7} />
+            {/* <DepthOfField
+            focusDistance={0.015}
+            focalLength={0.015}
+            bokehScale={0.5}
+            height={480}
+          /> */}
+            <ToneMapping mode={ToneMappingMode.ACES_FILMIC} resolution={256} />
+          </EffectComposer>
+        </Suspense>
       </Canvas>
     </>
   );
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import { useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls, Environment } from "@react-three/drei";
 import { Cathedral } from "./Cathedral";
+import DustParticles from "./DustParticles";
+import { Perf } from "r3f-perf";
 
 const Experience = () => {
   return (
     <>
       {/* <ambientLight intensity={2} color='#D8A35E' /> */}
+      <Perf position='top-left' />
       <OrbitControls />
       <Cathedral />
+      <DustParticles />
       <Environment files='/hdri/sunset.exr' background={true} />
     </>
   );
